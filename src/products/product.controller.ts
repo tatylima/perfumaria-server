85df46be5a12f0da8b6db1d2472435529017c869
Tatiana Gandra <tatianagandra78@gmail.com>
2022-09-20 22:04:37 -0300

    implantation swagger

--- src/products/product.controller.ts
+++ src/products/product.controller.ts
@@ -1,7 +1,9 @@
 import { Body, Controller, Get, Post } from '@nestjs/common';
+import { ApiTags } from '@nestjs/swagger';
 import { CreateProductDto } from './dto/create-product.dto';
 import { ProductService } from './product.service';
 
+@ApiTags('product')
 @Controller('product')
 export class ProductController {
   constructor(private productService: ProductService) {}
