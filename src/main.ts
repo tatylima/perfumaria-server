c0d267c4dee3f02c258cc1a48a1a837e7207a552
Tatiana Gandra <tatianagandra78@gmail.com>
2022-09-20 22:05:10 -0300

    implantation swagger

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,23 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.useGlobalPipes(new ValidationPipe());
+
+  const config = new DocumentBuilder()
+    .setTitle('Perfumaria')
+    .setDescription('Aplicação para gestão de pedidos')
+    .setVersion('1.0.0')
+    .addTag('status')
+    .addTag('product')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
 
   await app.listen(3333);
 }
--- src/products/dto/create-product.dto.ts
+++ src/products/dto/create-product.dto.ts
@@ -1,6 +1,11 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsNumber, IsPositive } from 'class-validator';
 export class CreateProductDto {
   @IsNumber()
   @IsPositive()
+  @ApiProperty({
+    description: 'O número do pedido',
+    example: 1,
+  })
   number: number;
 }
