839a028906167f5e3ecb67b69740c54ca60e1fc6
Tatiana Gandra <tatianagandra78@gmail.com>
2022-09-25 17:22:06 -0300

    create orden

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,10 @@ import { AppService } from './app.service';
 import { ProductModule } from './products/product.module';
 import { PrismaModule } from './prisma/prisma.module';
 import { UserModule } from './user/user.module';
+import { OrderModule } from './order/order.module';
 
 @Module({
-  imports: [ProductModule, PrismaModule, UserModule],
+  imports: [ProductModule, PrismaModule, UserModule, OrderModule],
   controllers: [AppController],
   providers: [AppService],
 })
