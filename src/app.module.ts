e15269ea5b9371fe410adf6da6eecf69a05fd8a4
Tatiana Gandra <tatianagandra78@gmail.com>
2022-09-25 16:33:33 -0300

    create user

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProductModule } from './products/product.module';
 import { PrismaModule } from './prisma/prisma.module';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [ProductModule, PrismaModule],
+  imports: [ProductModule, PrismaModule, UserModule],
   controllers: [AppController],
   providers: [AppService],
 })
